b26afa8afcf6d3685be143139c9355d3e6044467
Badr Echrami <tsokamotogamer@gmail.com>
2025-07-26 16:34:11 +0000

    Ensure the UI is fully responsive and mobile-friendly.
    - Navbar becomes

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -145,7 +145,7 @@ export default function SearchPage() {
         {!loading && searchResults.length > 0 && (
           <>
             <h3 className="text-xl font-semibold mb-4">{`Results for "${debouncedSearchTerm}"`}</h3>
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 md:gap-6">
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 md:gap-6 justify-items-center">
               {searchResults.map((book) => (
                 <div key={book.id} className="relative group">
                   <BookCard book={book} />
